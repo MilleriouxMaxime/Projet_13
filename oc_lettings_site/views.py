4ff1509ba025df5810c8f3c1b335d599d82a9814
MilleriouxMaxime <milleriouxmaxime@gmail.com>
2025-07-14 15:29:55 +0200

    add back missing comment

--- oc_lettings_site/views.py
+++ oc_lettings_site/views.py
@@ -6,6 +6,12 @@ from django.shortcuts import render
 logger = logging.getLogger(__name__)
 
 
+# Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie quam lobortis
+# leo consectetur ullamcorper non id est. Praesent dictum, nulla eget feugiat sagittis,
+# sem mi convallis eros, vitae dapibus nisi lorem dapibus sem. Maecenas pharetra purus
+# ipsum, eget consequat ipsum lobortis quis. Phasellus eleifend ex auctor venenatis tempus.
+# Aliquam vitae erat ac orci placerat luctus. Nullam elementum urna nisi, pellentesque
+# iaculis enim cursus in. Praesent volutpat porttitor magna, non finibus neque cursus id.
 def index(request):
     """Display the home page."""
     logger.info("Accessing home page view")
